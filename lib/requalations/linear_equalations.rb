ac331f737f4878524035e958f4a2c2a8d8dc391b
railsmaniac <iam@railsmaniac.com>
2009-05-10 02:29:27 +0400

    A good initizlize method for equalation class

--- lib/requalations/linear_equalations.rb
+++ lib/requalations/linear_equalations.rb
@@ -12,29 +12,30 @@ module Requalations
       attr_accessor :left_side_matrix, :right_side_matrix, :equalation_matrix 
       
       ## Class methods
-      
-      
-      ## initialize
+
+      ## Initializer
       
       # Initialize a new equalation object
+      # Just pass your equalation rows in an array into this
+      # And what you get will be separated matrices for coeficients and right side vector.
       # 
-      def initialize( a, b )
+      def initialize(equalations)
+        # Equalations are expected to be an array. 
+        # let's belive it is an array.
+        self.equalation_matrix = Matrix.rows( equalations )
         
-      end
-      
-      ## Instance methods
-      
-      # Solves the equalation using native DMatrix#solve
-      # 
-      def solve
-        return ::Linalg::DMatrix.solve( @equalation_coeficients, @results_vector)
-      end
-      
-      # Retrieves equalation matrix determinant
-      # a shorthand to @eaualation_coeficients.det
-      #
-      def det
-        @equalation_coeficients.det
+        # initialize our left and right sides
+        
+        # To initialize left side of our equalations array, we need to cut the last column from the whole equalation matrix. 
+        left_side_columns = self.equalation_matrix.column_vectors
+        left_side_columns.delete( left_side_columns.last )
+
+        # After that, we can use this to create lest side matrix
+        self.left_side_matrix = Matrix.columns( left_side_columns )
+        
+        # Right side of an equalation is a one-column matrix, or a vector. 
+        # Don't treat it as a row, it's really a column.
+        self.right_side_matrix = Matrix.column_vector( self.equalation_matrix.column_vectors.last )
       end
     end
   end
