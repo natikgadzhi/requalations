13ff6ac6c6bfc2406400a477fb6862f67f261400
railsmaniac <iam@railsmaniac.com>
2009-05-12 00:42:29 +0400

    Ротации

--- lib/requalations/matrix.rb
+++ lib/requalations/matrix.rb
@@ -28,6 +28,91 @@ module Requalations
     alias_method :set_element, :[]=
     alias_method :set_component, :[]= 
     
+    
+    # Find the matrix eigenvalues using rotations jacobi method
+    # 
+    def eigenvalues_using_rotations( eps = 0.1 )
+      # Подготовка
+      n = self.column_size
+      # матрица для обработки, первичная — равна этой. следующей итерации — по умолчанию, пустая. мы получим ее умножением. 
+      a = self.clone
+      a_next = ::Matrix.identity(n)
+      eigen_vectors = ::Matrix.identity(n)
+      
+      # кол-во итераций и критерий завершения процесса — сумма квадратов наддиагональных элементов
+      iterations_count = 0
+      elements_sum = 0
+      
+      begin
+        # Обновим итерационные данные
+        elements_sum = 0
+        iterations_count += 1
+        max_element_value = 0
+        
+        # 1) Найти наддиоганальный максимальный элемент
+        # Укоротить! 
+        for i in 0...n
+          for j in 0...n
+            if i < j 
+              if max_element_value < a[i,j].abs
+                max_element_value = a[i,j].abs
+                max_element_i = i
+                max_element_j = j
+              end
+            end
+          end
+        end
+    
+        # 2) Построить матрицу U вращения
+        # находим угол
+        if a[max_element_i, max_element_i] == a[max_element_j, max_element_j]
+          phi_angle = Math::PI/4
+        else
+          phi_angle = 0.5 * Math.atan(2 * max_element_value / (a[max_element_i, max_element_i] - a[max_element_j, max_element_j]))
+        end
+        # строим матрицу
+        u_matrix = ::Matrix.identity( n )
+        # диагональные
+        u_matrix[max_element_i, max_element_i] = u_matrix[max_element_j, max_element_j] = Math.cos(phi_angle)
+        # максимальный элемент и его симметричный
+        u_matrix[max_element_i, max_element_j] = - Math.sin(phi_angle)
+        u_matrix[max_element_j, max_element_i] = Math.sin(phi_angle)
+        
+        # puts u_matrix
+        #puts "<br><br><br>"
+        
+        # умножение и получение матрицы следующей итерации 
+        a_next = u_matrix.t * a * u_matrix
+        a = a_next.clone
+        
+        # А заодно матрицу собственных векторов умножим
+        eigen_vectors = eigen_vectors * u_matrix
+        
+        #puts "at this point we have: iteration #{iterations_count}, matrix #{a_next}<br><br>"
+        
+        # считаем сумму элементов
+        for i in 0...n
+          for j in 0...n
+            if i < j
+              elements_sum += a_next[i,j] ** 2
+            end
+          end
+        end 
+        elements_sum = elements_sum ** 0.5
+        
+        # Если сумма квадратов наддиагональных элементов больше точности — повторить. Иначе — выйти из цикла. 
+      end while elements_sum > eps 
+      
+      # После цикла, в a_next на диагонали лежат собственные значения исходной матрицы
+      @eigen_values = []
+      for i in 0...n do
+        @eigen_values[i] = a_next[i,i]
+      end
+      
+      # a_next
+      [ @eigen_values, eigen_vectors.column_vectors, iterations_count]
+    end
+    
     # Retrieves LU decomposition matrices for the matrix. 
     # 
     def lu
--- spec/requalations/matrix_spec.rb
+++ spec/requalations/matrix_spec.rb
@@ -21,6 +21,12 @@ describe Matrix do
       [ 2, 9, 6, -4],
       [ 5, 9, 1, 1]
     ])
+    
+    @eigen_matrix = Matrix.rows([
+      [ -7, -9, 1],
+      [ -9, 7, 2],
+      [ 1, 2, 9] 
+    ])
   end
   
   it "should be able to swap rows" do
@@ -42,13 +48,23 @@ describe Matrix do
     a.should == b
   end
   
-  it "should be able to get inverse matrix" do
-    inverse = @left_side_matrix.inverse_using_lup
+  # it "should be able to get inverse matrix" do
+  #   inverse = @left_side_matrix.inverse_using_lup
+  #   
+  #   puts @left_side_matrix.inverse
+  #   puts "<br>"
+  #   puts inverse
+  #   # (@left_side_matrix * inverse).should == Matrix::identity(@left_side_matrix.column_size)
+  # end
+  
+  it "should find eigenvectors" do
+    values, vectors, iterations =  @eigen_matrix.eigenvalues_using_rotations( 0.3 )
     
-    puts @left_side_matrix.inverse
+    puts values.to_a.map {|e| e.round }
     puts "<br>"
-    puts inverse
-    # (@left_side_matrix * inverse).should == Matrix::identity(@left_side_matrix.column_size)
+    puts "eigen vectors: #{vectors.map { |e| e.to_a.map{ |v| v.round }; "#{e}<br>"}}<br>"
+    
+    puts " in #{iterations} rotations"
   end
   
   
